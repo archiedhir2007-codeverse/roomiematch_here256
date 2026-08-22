79b17e47ed93a9c8b941dbf584ff8b47dec6e062
archiedhir2007-codeverse <archiedhir2007@gmail.com>
2026-08-22 17:01:32 +0530

    Create PageNotFound.jsx

---
+++ src/lib/PageNotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PageNotFound() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Page Not Found</h1>
+      <p>The page you're looking for doesn't exist.</p>
+    </div>
+  );
+}
